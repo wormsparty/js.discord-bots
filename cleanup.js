56aa6e06bdaa861e287daf8254d6f4c9a8b1e33a
mob <marc-olivier.bloch@elca.ch>
2020-08-18 11:10:41 +0200

    Cleanup bot looks like it's working great

--- cleanup.js
+++ cleanup.js
@@ -3,20 +3,30 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const config = require('./config.js');
 
+function fetchAndDeleteMessages(before) {
+    const channel = client.channels.cache.get(config.channels[0]);
+
+    channel.messages.fetch({ limit: 100, before: before }).then(messages => {
+        const mobsMessages = messages.filter(msg => msg.author.id === "387231361485766656").array();
+
+        if (mobsMessages.length > 0)
+        {
+            for(let i = 0 ; i < mobsMessages.length; i++) {
+                mobsMessages[i].delete();
+            }
+        }
+
+        if (messages.size > 0) {
+            fetchAndDeleteMessages(messages.last().id)
+        }
+
+    });
+}
+
 function run() {
     console.log(`cleanup-bot has started`);
 
-    var channel = client.channels.cache.get(config.channels[0]);
-    channel.messages.fetch({ limit: 100 }).then(messages => {
-        const mobsMessages = messages.filter(msg => msg.author.id === "387231361485766656").array();
-
-        for(var i = 0 ; i < mobsMessages.length; i++) {
-            console.log(i + ") " + mobsMessages[i].content);
-        }
-
-        console.log("Mob's messages = " + mobsMessages.length);
-        //channel.bulkDelete(botMessages);
-    });
+    fetchAndDeleteMessages(undefined);
 }
 
 module.exports = {
